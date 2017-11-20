dfa168fa01309516a8645fcd2e30a0cab383989d
stevenhalase@gmail.com <steven.halase@gmail.com>
2017-11-19 20:05:36 -0600

    Push

--- www/js/main.js
+++ www/js/main.js
@@ -458,6 +458,37 @@ Vue.component('leads-contact-overlay', {
   }
 })
 
+Vue.component('leads-nav-side', {
+  props: [],
+  template: `<div class="uk-card uk-card-default uk-card-body uk-width-1-2@s">
+              <ul class="uk-nav-default uk-nav-parent-icon" uk-nav>
+                  <li class="uk-active"><a href="#">Active</a></li>
+                  <li class="uk-parent">
+                      <a href="#">Parent</a>
+                      <ul class="uk-nav-sub">
+                          <li><a href="#">Sub item</a></li>
+                          <li><a href="#">Sub item</a></li>
+                      </ul>
+                  </li>
+                  <li class="uk-parent">
+                      <a href="#">Parent</a>
+                      <ul class="uk-nav-sub">
+                          <li><a href="#">Sub item</a></li>
+                          <li><a href="#">Sub item</a></li>
+                      </ul>
+                  </li>
+                  <li class="uk-nav-header">Header</li>
+                  <li><a href="#"><span class="uk-margin-small-right" uk-icon="icon: table"></span> Item</a></li>
+                  <li><a href="#"><span class="uk-margin-small-right" uk-icon="icon: thumbnails"></span> Item</a></li>
+                  <li class="uk-nav-divider"></li>
+                  <li><a href="#"><span class="uk-margin-small-right" uk-icon="icon: trash"></span> Item</a></li>
+              </ul>
+            </div>`,
+  methods: {
+    
+  }
+})
+
 let LeadsApp = new Vue({
   el: '#leadsApp',
   data: {
